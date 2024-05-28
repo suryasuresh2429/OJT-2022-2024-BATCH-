40513cafab9808461ba69a2e39086c0940d5eb1d
suryasuresh2429 <122439702+suryasuresh2429@users.noreply.github.com>
2024-05-28 13:41:38 +0530

    Add files via upload

---
+++ Day 8/standard_deviation.py
@@ -0,0 +1,5 @@
+#Practice the programs with Machine Learning - Standard Deviation using python Jupiter Notebook
+import statistics
+data = [10, 20, 30, 40, 50]
+sample_std_dev = statistics.stdev(data)
+print("Sample Standard Deviation: ", sample_std_dev)
