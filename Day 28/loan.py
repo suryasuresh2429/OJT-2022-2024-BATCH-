a423eb26b32813868f1c98ccbfd627659bc21c21
suryasuresh2429 <122439702+suryasuresh2429@users.noreply.github.com>
2024-06-26 17:14:42 +0530

    Add files via upload

---
+++ Day 28/loan.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score,classification_report,confusion_matrix,precision_score,recall_score,f1_score
+from sklearn.model_selection import GridSearchCV
+
+#load the dataset
+data=pd.read_csv('data.csv')
+x=data[['loan_amount','interest_rate','term','income','credit_score','age','employment_length']]
+y=data['loan_repaid']
+
+#train_tests
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+
+#initiate a model
+model=DecisionTreeClassifier(random_state=42)
+
+#train the model
+model.fit(x_train,y_train)
+
+#make a prediction
+y_pred=model.predict(x_test)
+
+#accuracy
+accuracy=accuracy_score(y_test,y_pred)
+print(f'accuracy:{accuracy:.2f}')  #'.2f' means it will only take the 2 floating point numbers
+print('classification_report :')
+print(classification_report(y_test,y_pred))
+
+#confusion matrix
+print("confusion matrix :")
+print(confusion_matrix(y_test,y_pred))
+
+#precision
+precision=precision_score(y_test,y_pred)
+print("precision:",precision)
+
+#recall
+recall=recall_score(y_test,y_pred)
+print("recall:",recall)
+
+#F1score
+f1score=f1_score(y_test,y_pred)
+print("F1 score:",f1score)
